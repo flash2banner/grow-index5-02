75841aa792ad400550e7d012e79ab0b12f5cad3c
flash2banner <77767535+flash2banner@users.noreply.github.com>
2022-08-02 19:33:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__content").addClass("active");
+  });
+
+  $(".header__close").click(function (event) {
+    $(".header__content").removeClass("active");
+  });
+
+  $(".main").click(function (event) {
+    if (
+      $(".header__content").hasClass("active") &&
+      event.target != $(".header__content")[0] &&
+      !$(".header__content").has(event.target).length
+    ) {
+      $(".header__content").removeClass("active");
+    }
+  });
+
+});
